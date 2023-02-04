c4a1053023e65d87208db3b6541cf0f13c0eaf60
TaufeqRazakh <taufeqmd@gmail.com>
2023-02-04 12:20:01 -0800

    added newer webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get "/", to: "recepe#index" 
+  root to: "recipes#index" 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
