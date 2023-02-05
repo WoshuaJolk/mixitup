e6a0eb633c4c49c517294c94ecbc564bb4b28a57
TaufeqRazakh <taufeqmd@gmail.com>
2023-02-04 17:20:58 -0800

    show works

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,18 @@
 class RecipesController < ApplicationController
-  def index
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe=Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(ingredients: "...", origin: "...", response: "...")
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: "recipes#index" 
+  root to: "recipes#new"
+  resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
